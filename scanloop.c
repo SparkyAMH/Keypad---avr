01c6d37388d291a10edb5c79cab9638400d5bc26
richardkarras <richard.karras@gmail.com>
2018-07-05 21:03:01 -0600

    Complete scan loop

--- scanloop.c
+++ scanloop.c
@@ -1,26 +1,32 @@
-for (j=0x0; j<0x3; j++)//set column - test row
-	{
-		//set column high
-		PORTD = j;
-		for (i=0x0; i<0x4; i++) //check for row
+char scanKey()
+{
+	for (j=0x0; j<0x3; j++)//set column - test row
 		{
-			temp = scankey(i);
-			if (temp != 0)
+			//set column high
+			PORTD = j;
+			for (i=0x00; i<0x04; i++) //check for row
 			{
-				KeyPressed = getCase(j,i); //send position for decoding
-				return (KeyPressed); //return pointer to array with column and row
+				temp = scankey(i);
+				if (temp != 0x00)
+				{
+					KeyPressed = getCase(j,i); //send position for decoding
+					return (KeyPressed); //return pointer to array with column and row
+				}
 			}
 		}
-	}
+		return 'Z';
+}
 
-	/* Keypad pattern r c
-	// 00 01 02		1 2 3
-	// 10 11 12		4 5 6
-	// 20 21 22		7 8 9
-	// 30 31 32		* 0 #
-	*/
-	int getCase(int column, int row)
+/* Keypad pattern r c
+// 00 01 02		1 2 3
+// 10 11 12		4 5 6
+// 20 21 22		7 8 9
+// 30 31 32		* 0 #
+*/
+char getCase(int column, int row)
 	{
+		char key = 'Q';
+
 		if (column = 0)
 		{
 			switch(row)
@@ -39,4 +45,41 @@ for (j=0x0; j<0x3; j++)//set column - test row
 					break;
 				} 
 		}	
+		if (column = 1)
+		{
+			switch(row)
+				 {
+					case 0x00 :
+					key = '2';
+					break;
+					case 0x01 :
+					key = '5';
+					break;
+					case 0x02 :
+					key = '8';
+					break;
+					case 0x03 :
+					key = '0';
+					break;
+				} 
+		}
+		if (column = 2)
+		{
+			switch(row)
+				 {
+					case 0x00 :
+					key = '3';
+					break;
+					case 0x01 :
+					key = '6';
+					break;
+					case 0x02 :
+					key = '9';
+					break;
+					case 0x03 :
+					key = '#';
+					break;
+				} 
+		}
+		return key;
 	}
